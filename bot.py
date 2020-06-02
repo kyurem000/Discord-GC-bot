a10743b338d067bd8e4c587fe375296e43e5bd4d
Divide <hanapsk61@knou.ac.kr>
2020-06-03 07:30:43 +0900

    python file

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import discord
-
-
 client = discord.Client()
 
 # 준비
@@ -8,16 +6,17 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("밤낮이 없어요")
-
+    game = discord.Game("heroku에서 조종중")
     # 현재 상태 나타내기
     await client.change_presence(status=discord.Status.online, activity=game)
-
 # 반응 이벤트
 @client.event
 async def on_message(message):
-    if message.content.startswith("!안녕"):
-        await message.channel.send("헬로우")
-
+    if message.content.startswith("!명령어"):
+        await message.channel.send("!인사, !뭐해")
+    if message.content.startswith("!인사"):
+        await message.channel.send("꾸벅")
+    if message.content.startswith("!뭐해"):
+        await message.channel.send("일해")
 # 토큰 보내기
 client.run("NzE3NDcwNzk1NzkzOTU2OTE1.XtbMwA.ibdtq2YkFqdKptC5LZsOvn4iCpk")
